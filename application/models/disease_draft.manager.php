c05f80e2a0cd89c2b7e8f77e1f9e7ddad9a420b0
samsep101 <artyommakhmutov85@gmail.com>
2023-12-03 15:37:08 +0500

    откат неудачной попытки логгера

--- application/models/disease_draft.manager.php
+++ application/models/disease_draft.manager.php
@@ -70,20 +70,20 @@ class DiseaseDraftManager extends ModelManager
 
         return false;
     }
-//    public function log($content){
-//        $file = fopen('/home/vhost/medbook/www/l.txt', 'a');
-//        fwrite($file, $content);
-//        fclose($file);
-//    }
+    public function log($content){
+        $file = fopen('/home/vhost/medbook/www/l.txt', 'a');
+        fwrite($file, $content);
+        fclose($file);
+    }
 
     protected function cloneToRelatedDisease(DiseaseDraftModel $diseaseDraftModel, DiseaseModel $diseaseModel)
     {
 
         echo 1123;
-//        $this->log(print_r($diseaseModel, 1));
-//        $this->log(print_r($diseaseDraftModel, 1));
+        $this->log(print_r($diseaseModel, 1));
+        $this->log(print_r($diseaseDraftModel, 1));
 
-//        $diseaseModel->title = $diseaseDraftModel->title;
+        $diseaseModel->title = $diseaseDraftModel->title;
         $diseaseModel->genitive_name = $diseaseDraftModel->genitive_name;
         $diseaseModel->prepositional_name = $diseaseDraftModel->prepositional_name;
         $diseaseModel->content = $diseaseDraftModel->content;
